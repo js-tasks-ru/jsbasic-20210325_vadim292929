965ee060d544cf8ee7b66a8d7c69eee3ae57668c
Bini29 <vadim292929@gmail.com>
2021-04-12 17:51:37 +0300

    Третий модуль

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,9 @@
+let arr = [5, 3, 8, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newarr = arr.slice([a],[b + 1]);
+
+    return newarr;
 }
+
+console.log(filterRange(arr, 0 , 2));
