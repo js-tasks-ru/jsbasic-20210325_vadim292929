74e73674547343ab519b05e8974c410538ac3e72
Bini229 <vadim292929@gmail.com>
2021-04-13 08:13:44 +0300

    Задача 3-2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,28 @@
 let arr = [5, 3, 8, 1];
 
 function filterRange(arr, a, b) {
-  let newarr = arr.slice([a],[b + 1]);
 
-    return newarr;
+  let newarr = arr.slice([a],[b]);
+
+  console.log(newarr); 
+
+  arr.forEach(element => {
+
+    if (element === a) {
+
+      console.log("dsfsd");
+
+    }
+    if (element === b) {
+
+      newarr.push(element);
+      
+    }
+  });
 }
+let filtered = filterRange(arr, 1, 4); 
 
-console.log(filterRange(arr, 0 , 2));
+
+console.log(filterRange(arr, 1, 4));
+console.log( filtered ); // [3,1] (совпадающие значения)
+console.log( arr ); // [5,3,8,1] (без изменений)
