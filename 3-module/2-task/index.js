31976b7621adc3a96e7aa62efd05b5d73d560154
Bini29 <vadim292929@gmail.com>
2021-04-15 09:59:56 +0300

    Третий модуль

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -2,27 +2,15 @@ let arr = [5, 3, 8, 1];
 
 function filterRange(arr, a, b) {
 
-  let newarr = arr.slice([a],[b]);
-
-  console.log(newarr); 
+  let newarr = [];
 
   arr.forEach(element => {
 
-    if (element === a) {
-
-      console.log("dsfsd");
-
-    }
-    if (element === b) {
+    if (element >= a && element <= b) {
 
       newarr.push(element);
-      
+
     }
   });
+   return newarr;
 }
-let filtered = filterRange(arr, 1, 4); 
-
-
-console.log(filterRange(arr, 1, 4));
-console.log( filtered ); // [3,1] (совпадающие значения)
-console.log( arr ); // [5,3,8,1] (без изменений)
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,39 @@
 function getMinMax(str) {
-  // ваш код...
+
+  let a = str.split(",").join(" ");
+
+  let b = a.split(" ");
+
+  let nubers = [];
+
+  for (const i of b) {
+
+    let n = Number(i);
+
+    if (!isNaN(n) ) {
+
+      nubers.push(n);
+    }
+  }
+
+  function compare(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+
+  let sortnum = nubers.sort(compare);
+
+  let fnum = sortnum.shift(0);
+
+  let snum = sortnum.pop(0);
+
+  let minMaxNumbers = {
+    min: fnum,
+    max: snum,
+  };
+
+  return minMaxNumbers;
+
 }
+
