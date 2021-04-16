57d06f276f4ce56172ad3861f559ee35e1f8cc03
Bini229 <vadim292929@gmail.com>
2021-04-16 08:04:14 +0300

    4 модуль 1-2 задача

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement("ul");
+
+  for (const i of friends) {
+    let listItem = document.createElement("li");
+
+    list.appendChild(listItem);
+
+    listItem.innerHTML += i.firstName + " " + i.lastName;
+  }
+
+  return list;
 }
