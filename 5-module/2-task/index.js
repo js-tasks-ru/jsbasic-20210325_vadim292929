af97ff05040b06a46a9cb0392d393b2667a27445
Bini229 <vadim292929@gmail.com>
2021-04-18 21:29:04 +0300

    5 - модуль

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector(".toggle-text-button");
+  let texthid = document.querySelector("#text");
+  btn.addEventListener("click", function () {
+
+
+    texthid.toggleAttribute("hidden");
+
+
+  });
+
 }
