4898ae84fca49a288f70fadd6eab067fa4696ced
Bini229 <vadim292929@gmail.com>
2021-04-18 19:12:21 +0300

    4 модуль

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,41 @@
 function highlight(table) {
+  let rows = table.rows;
 
+
+  for (const t of table.tBodies) {
+    let trows = t.rows;
+
+    for (const i of trows) {
+
+      let st = i;
+
+      let el = st.lastElementChild;
+
+      if (el.getAttribute("data-available") == "true") {
+        st.classList.add("available");
+
+      }
+      if (el.getAttribute("data-available") == "false") {
+        st.classList.add("unavailable");
+
+      }
+      if (el.hasAttribute("data-available") == false) {
+        st.setAttribute("hidden", "");
+
+      }
+
+      for (const el of i.children) {
+        if (el.innerHTML == "m") {
+          st.classList.add("male");
+        }
+        if (el.innerHTML == "f") {
+          st.classList.add("female");
+        }
+        if (el.innerHTML < "18") {
+          st.style.textDecoration = "line-through";
+        }
+
+      }
+    }
+  }
 }
